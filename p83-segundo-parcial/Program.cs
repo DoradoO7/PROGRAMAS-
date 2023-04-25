d241416f43b334942ef20fb9fb97fbc85eea41a2
DoradoO7 <36176576@uaz.edu.mx>
2023-04-25 11:21:31 -0600

    hjhh

---
+++ p83-segundo-parcial/Program.cs
@@ -0,0 +1,48 @@
+﻿int[] calificaciones = new int[100], mayor = new int[100];
+int num, suma = 0, tamj = 0, calif, veces = 0;
+float promedio = 0.0f;
+char resultado;
+do {
+    suma = 0; veces = 0;
+    Console.Clear();
+    Console.Write("Cuantos elementos deseas capturar? ");
+    num = int.Parse(Console.ReadLine());
+    for(int i = 0; i < num; i++){
+        Console.Write($"Elemento {i+1}: ");
+        calificaciones[i] = int.Parse(Console.ReadLine());
+        suma += calificaciones[i];
+    }
+    Console.WriteLine("\nLos elementos del arreglo son:");
+    for(int i = 0; i < num; i++){
+        Console.Write($"{calificaciones[i]} ");
+    }
+    promedio = (float)suma / (float)num;
+    Console.WriteLine($"\n\nEl promedio de las calificaciones es: \n{promedio}");
+    for(int i = 0, j = 0; i < num; i++){
+        if(calificaciones[i] > promedio){
+            mayor[j] = calificaciones[i];
+            j++;
+        }
+        tamj = j;
+    }    
+    Console.WriteLine("\nLas calificaciones mayores al promedio son: ");
+    for(int i = 0; i < tamj; i++){
+        Console.Write($"{mayor[i]} ");
+        mayor[i] = 100 - mayor[i];
+    }
+    Console.WriteLine("\n\nEl complemento a 100 es:");
+    for(int i = 0; i < num; i++){
+        Console.Write($"{100 - calificaciones[i]} ");
+    }
+    Console.WriteLine("\n\nContar calificacion:");
+    Console.Write("Cual calificacion? ");
+    calif = int.Parse(Console.ReadLine());
+    for(int i = 0; i < num; i++){
+        if(calificaciones[i] == calif)veces++;
+    }
+    Console.WriteLine($"El {calif} aparece {veces} veces");
+
+    Console.Write("\nQuieres repetir (S/N)? ");
+    resultado = char.ToUpper(Console.ReadLine()[0]);
+} while( resultado != 'N');
+Console.WriteLine("\nProceso Terminado...");
