35ebd25f477489a4f9f2667685d4a36729e87e44
DoradoO7 <36176576@uaz.edu.mx>
2023-05-02 11:46:28 -0600

    ll

--- p85-cuenta-bancaria-v1/Banco.cs
+++ p85-cuenta-bancaria-v1/Banco.cs
@@ -1,12 +1,9 @@
-public class Banco{
-    public string Nombre {get;set;}
-    public string Domicilio{get;set;}
-    public List<Cliente> Clientes{get;set;}
-
-    public Banco() => Clientes = new List<Cliente>();
-    public Banco(string nombre,string domicilio): this() =>(Nombre,Domicilio) = (nombre,domicilio);
-
-    public void AgregarCliente(Cliente cliente) => Clientes.Add(cliente);
-
-    public override string ToString() => $"{Nombre},{Domicilio}";
+public class Banco {
+public string Nombre {get; private set;}
+public string Direccion {get; private set;}
+public List<Cliente> Clientes {get; private set;}
+public Banco() => Clientes = new List<Cliente>();
+public Banco(string nombre, string domicilio) : this() => (Nombre,Direccion) = (nombre,domicilio);
+public void AgregarCliente(Cliente cliente) => Clientes.Add(cliente);
+public override string ToString() => $"{Nombre}, {Direccion}";
 }
